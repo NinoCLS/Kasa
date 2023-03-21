8a78bb5e53015c69a782bcec43d2fb696b706896
NinoCLS <91659996+NinoCLS@users.noreply.github.com>
2023-03-21 16:57:21 +0100

    notFound page

--- kasa/src/pages/notFound/notFound.jsx
+++ kasa/src/pages/notFound/notFound.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import "./notFound.scss";
+import Header from "../../components/Header/Header";
+import { Link } from "react-router-dom";
 
-const notFound = () => {
-	return (
-		<div>
-			
-		</div>
-	);
+const NotFound = () => {
+    return (
+        <div className="notFound">
+            <Header />
+            <div className="notFound_infos">
+                <h1 className="notFound_infos_title">404</h1>
+                <p className="notFound_infos_content">
+                    La page que vous recherchez n'existe pas.
+                </p>
+            </div>
+            <Link className="notFound_infos_return" to="/">
+                Retourner sur la page d'accueil
+            </Link>
+        </div>
+    );
 };
 
-export default notFound;
+export default NotFound;
