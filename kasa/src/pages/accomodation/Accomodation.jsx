c65ba03e759790973f77febae15f399a80893811
NinoCLS <91659996+NinoCLS@users.noreply.github.com>
2023-03-27 16:15:29 +0200

    save accomodation

--- kasa/src/pages/accomodation/Accomodation.jsx
+++ kasa/src/pages/accomodation/Accomodation.jsx
@@ -1,39 +1,45 @@
-import "./accomodation.scss";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import datas from "../../assets/api/housing.json";
+
 import Header from "../../components/Header/Header";
 import Slider from "../../components/Carrousel/Carrousel";
 import Footer from "../../components/Footer/Footer";
 import Dropdown from "../../components/Dropdown/Dropdown";
+import datas from "../../assets/api/housing.json";
 import greyStar from "../../assets/img/Pages/accomodation/Star.png";
 import redStar from "../../assets/img/Pages/accomodation/RedStar.png";
 
-const Accommodation = () => {
+import "./accomodation.scss";
+
+export default function Accomodation() {
     const [imageSlider, setImageSlider] = useState([]);
-    const { id } = useParams();
-    const currentAccommodationData = datas.filter((data) => data.id === id)[0];
+    const { idAccomodation } = useParams();
+    const [dataCurrentAccomodation, setDataCurrentAccomodation] = useState({});
     const {
-        title,
-        location,
-        tags,
-        host: { name, picture },
-        rating,
-        description,
-        equipments,
-    } = currentAccommodationData;
+        title = "",
+        location = "",
+        tags = [],
+        host: { name = "", picture = "" } = {},
+        rating = 0,
+        description = "",
+        equipments = "",
+    } = dataCurrentAccomodation;
+    const [firstName, lastName] = name.split(" ");
 
     useEffect(() => {
-        setImageSlider(currentAccommodationData.pictures);
-    }, [id, currentAccommodationData]);
+        const currentAccomodation =
+            datas.find((data) => data.id === idAccomodation) || {};
+        setImageSlider(currentAccomodation.pictures || []);
+        setDataCurrentAccomodation(currentAccomodation);
+    }, [idAccomodation]);
 
     return (
         <>
             <Header />
             <Slider imageSlider={imageSlider} />
-            <main className="accommodation">
-                <div className="accommodation_content">
-                    <div className="accommodation_content_infos">
+            <main className="accomodation">
+                <div className="accomodation_content">
+                    <div className="accomodation_content_infos">
                         <h1>{title}</h1>
                         <p>{location}</p>
                         <div>
@@ -42,40 +48,37 @@ const Accommodation = () => {
                             ))}
                         </div>
                     </div>
-                    <div className="accommodation_content_host">
+                    <div className="accomodation_content_host">
                         <div>
-                            <div className="accommodation_content_host_name">
-                                <span>{name.split(" ")[0]}</span>
-                                <span>{name.split(" ")[1]}</span>
+                            <div className="accomodation_content_host_name">
+                                <span>{firstName}</span>
+                                <span>{lastName}</span>
                             </div>
                             <img
                                 src={picture}
-                                alt="host of this accommodation"
+                                alt="host of this accomodation"
                             />
                         </div>
-                        <div className="accommodation_content_host_stars">
-                            {[...Array(5)].map((_, index) => {
-                                const ratingValue = index + 1;
-                                return (
-                                    <img
-                                        key={index}
-                                        src={
-                                            ratingValue <= rating
-                                                ? redStar
-                                                : greyStar
-                                        }
-                                        alt="star"
-                                    />
-                                );
-                            })}
+
+                        <div className="accomodation_content_host_stars">
+                            {/* affiche le bon nombre d'etoile rouge ou grise en fonction du rating */}
+                            {[...Array(5)].map((_, index) => (
+                                <img
+                                    key={index}
+                                    src={
+                                        index + 1 <= rating ? redStar : greyStar
+                                    }
+                                    alt="star"
+                                />
+                            ))}
                         </div>
                     </div>
                 </div>
-                <div className="accommodation_dropdown">
-                    <div className="accommodation_dropdown_item">
+                <div className="accomodation_dropdown">
+                    <div className="accomodation_dropdown_item">
                         <Dropdown title={"Description"} content={description} />
                     </div>
-                    <div className="accommodation_dropdown_item">
+                    <div className="accomodation_dropdown_item">
                         <Dropdown title={"Équipements"} content={equipments} />
                     </div>
                 </div>
@@ -83,6 +86,4 @@ const Accommodation = () => {
             <Footer />
         </>
     );
-};
-
-export default Accommodation;
+}
