f17457cdc8d2523d86b210813cb29ac1fd23a437
NinoCLS <91659996+NinoCLS@users.noreply.github.com>
2023-03-29 16:21:33 +0200

    debug error id in accomodation pages

--- kasa/src/pages/accomodation/Accomodation.jsx
+++ kasa/src/pages/accomodation/Accomodation.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import NotFound from "../notFound/notFound";
 
 import Header from "../../components/Header/Header";
 import Slider from "../../components/Carrousel/Carrousel";
@@ -32,57 +33,79 @@ const Accomodation = () => {
         setDataCurrentAccomodation(currentAccomodation);
     }, [id]);
 
+    // if (Object.keys(dataCurrentAccomodation).length === 0) {
+    //     return (
+    //         <>
+    //             <NotFound />
+    //         </>
+    //     );
+    // }
+
     return (
         <>
-            <Header />
-            <Slider imageSlider={imageSlider} />
-            <main className="accomodation">
-                <div className="accomodation_content">
-                    <div className="accomodation_content_infos">
-                        <h1>{title}</h1>
-                        <p>{location}</p>
-                        <div>
-                            {tags.map((tag, index) => (
-                                <button key={index}>{tag}</button>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="accomodation_content_host">
-                        <div>
-                            <div className="accomodation_content_host_name">
-                                <span>{firstName}</span>
-                                <span>{lastName}</span>
+            {Object.keys(dataCurrentAccomodation).length === 0 ? (
+                <NotFound />
+            ) : (
+                <>
+                    <Header />
+                    <Slider imageSlider={imageSlider} />
+                    <main className="accomodation">
+                        <div className="accomodation_content">
+                            <div className="accomodation_content_infos">
+                                <h1>{title}</h1>
+                                <p>{location}</p>
+                                <div>
+                                    {tags.map((tag, index) => (
+                                        <button key={index}>{tag}</button>
+                                    ))}
+                                </div>
                             </div>
-                            <img
-                                src={picture}
-                                alt="host of this accomodation"
-                            />
-                        </div>
+                            <div className="accomodation_content_host">
+                                <div>
+                                    <div className="accomodation_content_host_name">
+                                        <span>{firstName}</span>
+                                        <span>{lastName}</span>
+                                    </div>
+                                    <img
+                                        src={picture}
+                                        alt="host of this accomodation"
+                                    />
+                                </div>
 
-                        <div className="accomodation_content_host_stars">
-                            {/* affiche le bon nombre d'etoile rouge ou grise en fonction du rating */}
-                            {[...Array(5)].map((_, index) => (
-                                <img
-                                    key={index}
-                                    src={
-                                        index + 1 <= rating ? redStar : greyStar
-                                    }
-                                    alt="star"
-                                />
-                            ))}
+                                <div className="accomodation_content_host_stars">
+                                    {/* affiche le bon nombre d'etoile rouge ou grise en fonction du rating */}
+                                    {[...Array(5)].map((_, index) => (
+                                        <img
+                                            key={index}
+                                            src={
+                                                index + 1 <= rating
+                                                    ? redStar
+                                                    : greyStar
+                                            }
+                                            alt="star"
+                                        />
+                                    ))}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="accomodation_dropdown">
-                    <div className="accomodation_dropdown_item">
-                        <Dropdown title={"Description"} content={description} />
-                    </div>
-                    <div className="accomodation_dropdown_item">
-                        <Dropdown title={"Équipements"} content={equipments} />
-                    </div>
-                </div>
-            </main>
-            <Footer />
+                        <div className="accomodation_dropdown">
+                            <div className="accomodation_dropdown_item">
+                                <Dropdown
+                                    title={"Description"}
+                                    content={description}
+                                />
+                            </div>
+                            <div className="accomodation_dropdown_item">
+                                <Dropdown
+                                    title={"Équipements"}
+                                    content={equipments}
+                                />
+                            </div>
+                        </div>
+                    </main>
+                    <Footer />
+                </>
+            )}
         </>
     );
 };
